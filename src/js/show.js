ae896838285140593246c5803db05906de91219c
solomon234 <solomonmuratov@gmail.com>
2018-11-14 14:07:07 -0500

    changes were made
    working on javascript since mock up is complete

---
+++ src/js/show.js
@@ -0,0 +1,8 @@
+function showList() {
+    var x = document.getElementById("showFrames");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
