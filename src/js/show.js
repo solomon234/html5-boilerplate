72f7d7f4382c98be63d2b2a9eb883080a2e76a10
solomon234 <solomonmuratov@gmail.com>
2018-11-16 14:32:30 -0500

    js and css changes

--- src/js/show.js
+++ src/js/show.js
@@ -1,9 +1,15 @@
 jQuery(document).ready(function() {
-  $("#fType").click(function() {
+
+  var showLStyle = jQuery("#showLStyle");
+  var fType = jQuery("#fType");
+  var lStyle = jQuery("#lStyle");
+  var steps = jQuery(".step");
+  var form = $('#sign-form');
+  fType.click(function() {
     $("#showFrames").toggle("fast");
   });
-  $("#lStyle").click(function() {
-    $("#showLStyle").toggle("fast");
+  lStyle.click(function() {
+
   });
   $("#progLTech").click(function() {
     $("#showPLT").toggle("fast");
@@ -15,4 +21,30 @@ jQuery(document).ready(function() {
     $("#showLAddOn").toggle("fast");
   });
 
+
+// Moves progress bar to next
+  lStyle.bind("click", function() {
+    showLStyle.toggle("fast");
+    jQuery.each(steps, function(i) {
+      if (!jQuery(steps[i]).hasClass('current') && !jQuery(steps[i]).hasClass('done')) {
+        jQuery(steps[i]).addClass('current');
+        jQuery(steps[i - 1]).removeClass('current').addClass('done');
+        return false;
+      }
+
+    })
+  });
+ /* may need for loop to reset addclass
+
+  fType.bind("click", function() {
+    jQuery.each(steps, function(i) {
+      if (jQuery(steps[i+1]).hasClass('done')) {
+        jQuery(steps[i+1]).removeClass('done');
+        jQuery(steps[i+1]).removeClass('current');
+        jQuery(steps[i]).addClass('current').removeClass('done');
+        return false;
+      }
+    })
+  });
+*/
 });
