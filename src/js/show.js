56c514e5df70959a4378a4ffafac30e8bae77ad7
solomon234 <solomonmuratov@gmail.com>
2018-12-30 14:38:04 -0500

    Revert "progress bar and option changes"
    This reverts commit 0d64ef3f08fdfda2f213ddd145b5f6d8436d2f5d.

--- src/js/show.js
+++ src/js/show.js
@@ -13,23 +13,19 @@ jQuery(document).ready(function() {
 
     $('.lensImg li .optionContainer').on('click', function() {
       if (!$(this).parent().hasClass('lensImgSelect')) {
+
+        /*       One option is selected / all other options are unselected           */
+        $(this).parent().addClass('lensImgSelect').siblings().removeClass('lensImgSelect');
+
+        /*       Declaring Variables to calcuate data               */
+        var data = $('.lensImgSelect .picText').text();
+        var price = $(".lensImgSelect .optionPrice");
         /* Read in id tag name*/
         var option = $(this).parent().parent().attr('id');
 
-        /*       One option is selected / all other options are unselected           */
-        if (option != 'showLAddOn') {
-          $(this).parent().addClass('lensImgSelect').siblings().removeClass('lensImgSelect');
-        } else {
-          $(this).parent().addClass('lensImgSelect');
-        }
-        /*       Declaring Variables to calcuate data               */
+        /* debugging */
+        //console.log("%s is data.\n %s is price", data, price.text());
 
-        var data = $('.lensImgSelect .picText').text();
-        var price = $(".lensImgSelect .optionPrice");
-
-
-
-        /* price convert */
         if (price.length) {
           price = price.text().replace(/\D/g, '');
         } else {
@@ -41,37 +37,25 @@ jQuery(document).ready(function() {
           framePrice = parseInt(price);
           $(".frameData").text(data);
           $('#' + option).toggle().parent().next('li').toggle().find('ol').toggle('fast');
-          steps.after("<div class='step current' > Lens Style </div>");
-          steps = steps.nextAll(':first');
+          steps = steps.nextAll(':first').addClass('current');
         } else if (option == 'showLStyle') {
           lenStylePrice = parseInt(price);
           $(".lensData").text(data);
-          if (data == "Progressive") {
-            $('#' + option).toggle().parent().next('li').toggle().find('ol').toggle('fast');
-            steps.after("<div class='step current' > Progressive </div>");
-            steps = steps.nextAll(':first');
-          } else {
-            $('#' + option).toggle().parent().next('li').next('li').toggle().find('ol').toggle('fast');
-            steps.after("<div class='step current' > Lens Material </div>");
-            steps = steps.nextAll(':first');
-          }
-        } else if (option == 'showPLT') {
+          $('#' + option).toggle().parent().next('li').toggle().find('ol').toggle('fast');
+          steps = steps.nextAll(':first').addClass('current');        } else if (option == 'showPLT') {
           progressivePrice = parseInt(price);
           $(".progData").text(data);
           $('#' + option).toggle().parent().next('li').toggle().find('ol').toggle('fast');
-          steps.after("<div class='step current' > Lens Material </div>");
-          steps = steps.nextAll(':first');
+          steps = steps.nextAll(':first').addClass('current');
         } else if (option == 'showLMaterial') {
           lensMaterialPrice = parseInt(price);
           $(".materialData").text(data);
           $('#' + option).toggle().parent().next('li').toggle().find('ol').toggle('fast');
-          steps.after("<div class='step current' > Add-On </div>");
-          steps = steps.nextAll(':first');
+          steps = steps.nextAll(':first').addClass('current');
         } else if (option == 'showLAddOn') {
-          // Multiple add-ons selection
-          lensAddOnPrice += parseInt(price);
+          lensAddOnPrice = parseInt(price);
           $(".addOnData").text(data);
-          //$('#' + option).toggle().parent().next('li').toggle().find('ol').toggle('fast');
+          $('#' + option).toggle().parent().next('li').toggle().find('ol').toggle('fast');
           steps = steps.nextAll(':first').addClass('current');
         }
 
@@ -101,15 +85,15 @@ jQuery(document).ready(function() {
 
 
   // Moves progress bar to next
-  //
-  // jQuery.each(steps, function(i) {
-  //   if (!$(steps[i]).hasClass('current') && !$(steps[i]).hasClass('done')) {
-  //     $(steps[i]).addClass('current');
-  //     $(steps[i - 1]).removeClass('current').addClass('done');
-  //     return false;
-  //   }
-  //
-  // })
+    //
+    // jQuery.each(steps, function(i) {
+    //   if (!$(steps[i]).hasClass('current') && !$(steps[i]).hasClass('done')) {
+    //     $(steps[i]).addClass('current');
+    //     $(steps[i - 1]).removeClass('current').addClass('done');
+    //     return false;
+    //   }
+    //
+    // })
   /* may need for loop to reset addclass
 
   fType.bind("click", function() {
